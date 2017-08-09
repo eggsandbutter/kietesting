8fa650713cbcb7ac4a7b5ea1c3934e8a52182b07
eggsandbutter <rudyatleti@gmail.com>
2017-08-09 16:53:42 +0200

    commit

--- KieTesting/src/test/PublishContainer.java
+++ KieTesting/src/test/PublishContainer.java
@@ -12,7 +12,7 @@ public class PublishContainer {
 		String containerName = "KieContainer";
 		String artifact = "myProject";
 		String group = "myOrganization";
-		String version = "1.0.3";
+		String version = "1.0.5";
 		
 		CallKie.publishContainer(containerName, artifact, group, version);
 	}
